7998eb103bb4e53903803ddcc35f4298973a5968
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-15 00:56:19 +0100

    Beispiel 02

--- script_01.js
+++ script_01.js
@@ -1,102 +1,102 @@
 "use strict";
-// /***********************************************************************/
-// /*********                    Funktionen 02                   **********/
-// /***********************************************************************/
+/***********************************************************************/
+/*********                    Funktionen 02                   **********/
+/***********************************************************************/
 
-// /********* Rechner **********/
-// /*
-//     0. a+b / a-b/ a*b / a/b  // ergebnis c 
-//     1. Dateneingabe + -überprüfung :
-//     2. Auswahl Rechenart :  check!
-//     3. Fkt. Grundrechenarten : check!
-//     4. Ausgabe in Konsole : check!
-// */
+/********* Rechner **********/
+/*
+    0. a+b / a-b/ a*b / a/b  // ergebnis c 
+    1. Dateneingabe + -überprüfung :
+    2. Auswahl Rechenart :  check!
+    3. Fkt. Grundrechenarten : check!
+    4. Ausgabe in Konsole : check!
+*/
 
-// /***********************************************************************/
-// /*********                     Beispiel 01                    **********/
-// /***********************************************************************/
+/***********************************************************************/
+/*********                     Beispiel 01                    **********/
+/***********************************************************************/
 
-//     // application / app:
-//     // starApp();
-//     function starApp() {
-//         output1( calculate1( getNumber1("first"), getNumber1("second"), getOp1() ) );
-//     }
-//     // module: number input:
-//     function getNumber1( figure ) {
-//         let ipnutStr = prompt( "Please insert " + figure + " number: " );
-//         let num = Number( ipnutStr );
-//         return num;
-//     }
-//     // output1( getOp1() )
-//     // module: operator input:
-//     function getOp1() { 
-//         let op1 = prompt( "Please insert correct operator: " );
-//         //ist op1 gültig? true or false
-//         if ( isValidOp(op1) ) {
-//             return op1;
-//         } else {
-//             getOp1();
-//         }
-//     }
-//     // module: check if operator input is valid:
-//     function isValidOp( op1 ) {
-//         return op1 == "+" || op1 == "-" || op1 == "*" || op1 == "/" || op1 == ":";
-//     }
+    // application / app:
+    // starApp();
+    function starApp() {
+        output1( calculate1( getNumber1("first"), getNumber1("second"), getOp1() ) );
+    }
+    // module: number input:
+    function getNumber1( figure ) {
+        let ipnutStr = prompt( "Please insert " + figure + " number: " );
+        let num = Number( ipnutStr );
+        return num;
+    }
+    // output1( getOp1() )
+    // module: operator input:
+    function getOp1() { 
+        let op1 = prompt( "Please insert correct operator: " );
+        //ist op1 gültig? true or false
+        if ( isValidOp(op1) ) {
+            return op1;
+        } else {
+            getOp1();
+        }
+    }
+    // module: check if operator input is valid:
+    function isValidOp( op1 ) {
+        return op1 == "+" || op1 == "-" || op1 == "*" || op1 == "/" || op1 == ":";
+    }
 
-//     // module: calculator:
-//     // agreement: +, -, *, /, :
-//     // output1( calculate1( 2, 2, "+" ) );
-//     // output1( calculate1( 2, 2, "-" ) );
-//     // output1( calculate1( 2, 2, "*" ) );
-//     // output1( calculate1( 2, 2, "/" ) );
-//     // output1( calculate1( 2, 2, ":" ) );
-//     // output1( calculate1( 2, 2, """ ) );
-//     // output1( calculate1( 2, 2, "!" ) );
-//     function calculate1( a1, b1, op1 ) {
-//         switch ( op1 ) {
-//             case "+":                   // addition
-//                 return add1( a1, b1 );
-//             case "-":                   // subtraction
-//                 return sub1( a1, b1 );
-//             case "*":                   // mutliplication
-//                 return mul1( a1, b1 );
-//             case "/":                   // division1
-//             case ":":                   // division2
-//                 return div1( a1, b1 );
-//             default:                    // default
-//                 return "Error: insert wrong";
-//         }
-//     }
-//     // module: console output:
-//     // output1( "Test" );                  // test
-//     // output1( 2 );                       // test
-//     function output1( outputData1 ){ 
-//         if ( typeof outputData1 == "number" ) {
-//             console.log( "The result is: " + outputData1 );
-//         } else {
-//             console.log( "ERROR: " + outputData1 );
-//         }
-//     }
-//     // module: addition:
-//     // output1( add1(2,5) );                                           // test
-//     function add1( a1, b1 ) { 
-//         let c1 = a1 + b1;                                           // lange ausführliche Schreibweise
-//         return c1;                                                  // Ausleitung der Variable
-//     }
-//     // module: subtraction:
-//     // output1( sub1(5,2) );                                           // test
-//     function sub1( a1, b1 ) {
-//         return a1 - b1;                                             // schnelle Schreibweise und direkte Ausleitung
-//     }
-//     // module: division:
-//     // output1( div1(8,2) );                                           // test
-//     function div1( a1, b1 ) { 
-//         if ( b1 == 0 ) { return "Division by 0 not possible!"; }
-//         return a1 / b1; 
-//     }
-//     // module: multiplication:
-//     // output1( mul1(2,2) );                                           // test
-//     function mul1( a1, b1 ) { return a1 * b1; }                     // alles als Einzeiler
+    // module: calculator:
+    // agreement: +, -, *, /, :
+    // output1( calculate1( 2, 2, "+" ) );
+    // output1( calculate1( 2, 2, "-" ) );
+    // output1( calculate1( 2, 2, "*" ) );
+    // output1( calculate1( 2, 2, "/" ) );
+    // output1( calculate1( 2, 2, ":" ) );
+    // output1( calculate1( 2, 2, """ ) );
+    // output1( calculate1( 2, 2, "!" ) );
+    function calculate1( a1, b1, op1 ) {
+        switch ( op1 ) {
+            case "+":                   // addition
+                return add1( a1, b1 );
+            case "-":                   // subtraction
+                return sub1( a1, b1 );
+            case "*":                   // mutliplication
+                return mul1( a1, b1 );
+            case "/":                   // division1
+            case ":":                   // division2
+                return div1( a1, b1 );
+            default:                    // default
+                return "Error: insert wrong";
+        }
+    }
+    // module: console output:
+    // output1( "Test" );                  // test
+    // output1( 2 );                       // test
+    function output1( outputData1 ){ 
+        if ( typeof outputData1 == "number" ) {
+            console.log( "The result is: " + outputData1 );
+        } else {
+            console.log( "ERROR: " + outputData1 );
+        }
+    }
+    // module: addition:
+    // output1( add1(2,5) );                                           // test
+    function add1( a1, b1 ) { 
+        let c1 = a1 + b1;                                           // lange ausführliche Schreibweise
+        return c1;                                                  // Ausleitung der Variable
+    }
+    // module: subtraction:
+    // output1( sub1(5,2) );                                           // test
+    function sub1( a1, b1 ) {
+        return a1 - b1;                                             // schnelle Schreibweise und direkte Ausleitung
+    }
+    // module: division:
+    // output1( div1(8,2) );                                           // test
+    function div1( a1, b1 ) { 
+        if ( b1 == 0 ) { return "Division by 0 not possible!"; }
+        return a1 / b1; 
+    }
+    // module: multiplication:
+    // output1( mul1(2,2) );                                           // test
+    function mul1( a1, b1 ) { return a1 * b1; }                     // alles als Einzeiler
 
 /***********************************************************************/
 /*********                     Beispiel 02                    **********/
