fa28889f8de0863c976f6c526887c4b1e2d18e19
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-15 00:55:34 +0100

    Beispiel 02

--- script_01.js
+++ script_01.js
@@ -101,43 +101,63 @@
 /***********************************************************************/
 /*********                     Beispiel 02                    **********/
 /***********************************************************************/
-    // const userInput2 = prompt( "Gib eine Rechnung ein:\nBeispiele:\n[ 2*3 ], [ 7 mal 8 ], [ 9 / 5 ] " );
-    const userInput2 = "4mal5";
+    const userInput2 = prompt( "Gib eine Rechnung ein:\nBeispiele:\n[ 2*3 ], [ 7 mal 8 ], [ 9 / 5 ] " );
+    // const userInput2 = "40 mal 5";
     const addOp2 = [ "+", "add", " add ", "plus", " plus " ];
     const subOp2 = [ "-", "minus", " minus ", "subtract", " subtract " ];
     const mulOp2 = [ "*", "mal", " mal ", "multiply", " multiply " ];
     const divOp2 = [ "/", ":", "geteilt", " geteilt ", "geteilt durch", " geteilt durch ", "divide", " divide ", "divide by", " divide by " ];
     const opArr2 = [ ...addOp2, ...subOp2, ...mulOp2, ...divOp2 ];
 
-    output2( checkOp2( userInput2 ) );
+    calculate2( userInput2 );
 
     function output2( o2 ){ console.log( o2 ); }
 
+    function calculate2( userInput2 ) {
+        let calcVal = checkOp2( userInput2 );
+        if ( calcVal.calcOpName == "Division" && calcVal.numberTwo == 0 ) {
+            output2( "Division by 0 not possible!" );
+        } else {
+            if ( calcVal === false ){
+                output2( "Not the correct operator used" );
+                output2( "No Result" );
+            } else {
+                output2( calcVal.calcOpName + ": " + calcVal.numberOne + " " + calcVal.calcOperator + " " + calcVal.numberTwo );
+                output2( "Result: " + ( calcVal.opFunc(calcVal.numberOne, calcVal.numberTwo) ) );
+            }
+        }
+    }
+
     function checkOp2( userInput2 ) {
-        let resOp = "Not the correct operator used";
+        let resOp = false;
         opArr2.forEach( op2 => {
             if ( userInput2.indexOf( op2 ) > -1 ) {
-                let calcOperator, calcOpName;
+                let calcOperator, calcOpName, opFunc;
                 let inputSplit = userInput2.split( op2 );
                 let numberOne = Number( inputSplit[ 0 ].replace( ",", "." ) );
                 let numberTwo = Number( inputSplit[ 1 ].replace( ",", "." ) );
                 if ( op2 == "+" || op2 == "add" || op2 == " add " || op2 == "plus" || op2 == " plus " ) {
                     calcOperator = "+";
                     calcOpName = "Addition";
+                    opFunc = function( a, b ){ return a+b }
                 } else if ( op2 == "-" || op2 == "minus" || op2 == " minus " || op2 == "subtract" || op2 == " subtract " ) {
                     calcOperator = "-";
                     calcOpName = "Subtraction";
+                    opFunc = function( a, b ){ return a-b }
                 } else if ( op2 == "*" || op2 == "mal" || op2 == " mal " || op2 == "multiply" || op2 == " multiply " ) {
                     calcOperator = "*";
                     calcOpName = "Multiplication";
+                    opFunc = function( a, b ){ return a*b }
                 } else if ( op2 == "/" || op2 == ":" || op2 == "geteilt" || op2 == " geteilt " || op2 == "geteilt durch" || op2 == " geteilt durch " || op2 == "divide" || op2 == " divide " || op2 == "divide by" || op2 == " divide by " ) {
-                    calcOperator = "/";
+                    calcOperator = ":";
                     calcOpName = "Division";
+                    opFunc = function( a, b ){ return a/b }
                 } else {
-                    calcOperator = "Not the correct operator used";
-                    calcOpName = "Not the correct operator used";
+                    calcOperator = false;
+                    calcOpName = false;
+                    opFunc = false;
                 }  
-                resOp = { calcOperator, numberOne, numberTwo, calcOpName };
+                resOp = { calcOperator, numberOne, numberTwo, calcOpName, opFunc };
             }
         });
         return resOp;
