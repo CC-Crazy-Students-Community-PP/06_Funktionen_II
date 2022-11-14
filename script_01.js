42150f80a80b814921eefb7d45beb150a29820ae
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-14 23:51:18 +0100

    Beispiel 02 a

--- script_01.js
+++ script_01.js
@@ -101,59 +101,35 @@
 /***********************************************************************/
 /*********                     Beispiel 02                    **********/
 /***********************************************************************/
-    // let userInput2 = prompt( "Gib eine Rechnung ein:\nBeispiele:\n[ 2*3 ], [ 7 mal 8 ], [ 9 / 5 ] " );
-    // getInput2( "5+8" );
-
+    // const userInput2 = prompt( "Gib eine Rechnung ein:\nBeispiele:\n[ 2*3 ], [ 7 mal 8 ], [ 9 / 5 ] " );
+    const userInput2 = "4+5";
     const addOp2 = [ "+", "add", " add ", "plus", " plus " ];
     const subOp2 = [ "-", "minus", " minus ", "subtract", " subtract " ];
     const mulOp2 = [ "*", "mal", " mal ", "multiply", " multiply " ];
     const divOp2 = [ "/", ":", "geteilt", " geteilt ", "geteilt durch", " geteilt durch ", "divide", " divide ", "divide by", " divide by " ];
+    const opArr2 = [ ...addOp2, ...subOp2, ...mulOp2, ...divOp2 ];
 
-    function calculate2( userInputArray2, opElement2 ) {
-        let isOpValid = isValidOp( opElement2 );
-        if ( isOpValid ) {
-            let opArr = isValidOp( opElement2 )[ 0 ];
-            let opAct = isValidOp( opElement2 )[ 1 ];
-            let opNom = isValidOp( opElement2 )[ 2 ];
-            output2( opNom + "\na) " + userInputArray2 + "\nb) " + opElement2 );
-            output2( "Output-2:\na) " + opArr + "\nb) " + opAct );
-        } else {
-            return "Inputerror, not a correct operator used!";
-        }
-    }
+    output2( checkOp2( userInput2 ) );
 
-    // module: get users input data
-    output2( getInput2( "5 & 5" ) );
-    function getInput2( userInput2 ) {
-        let opArray2 = [ ...addOp2, ...subOp2, ...mulOp2, ...divOp2 ];
-        opArray2.forEach( opElement2 => {
-            if ( userInput2.indexOf( opElement2 ) != -1 ) {
-                const userInputArray2 = userInput2.split( opElement2 );
-                calculate2( userInputArray2, opElement2 );
-            } else {
-                return "2 Inputerror, not a correct operator used!";
-            }
+    function output2( o2 ){ console.log( o2 ); }
+
+    function checkOp2( userInput2 ) {
+        let resOp;
+        opArr2.forEach( op2 => {
+            let inputSplit = userInput2.split( op2 );
+            let numberOne = Number( inputSplit[ 0 ] );
+            let numberTwo = Number( inputSplit[ 1 ] );
+            let opIsInString = userInput2.indexOf( op2 ) > -1;
+        
+            resOp = { inputSplit, op2, numberOne, numberTwo, opIsInString };
         });
+        return resOp;
+        // opArr2.forEach( op2 => {
+        //     let resultOp12 = userInput2.split( op2 );
+        //     let resultOp22 = ( userInput2.indexOf( op2 ) > -1 );
+        //     return [ resultOp12, resultOp22 ];
+        // });
     }
-
-    // module: check if operator input is valid
-    // output2( isValidOp( " subtract " ) );
-    function isValidOp( op2 ) {
-        if ( op2 == "+" || op2 == "add" || op2 == " add " || op2 == "plus" || op2 == " plus " ) {
-            return [ addOp2, "+", "Addition: " ];
-        } else if ( op2 == "-" || op2 == "minus" || op2 == " minus " || op2 == "subtract" || op2 == " subtract " ) {
-            return [ subOp2, "-", "Subtraction: " ];
-        } else if ( op2 == "*" || op2 == "mal" || op2 == " mal " || op2 == "multiply" || op2 == " multiply " ) {
-            return [ mulOp2, "*", "Multiplication: " ];
-        } else if ( op2 == "/" || op2 == ":" || op2 == "geteilt" || op2 == " geteilt " || op2 == "geteilt durch" || op2 == " geteilt durch " || op2 == "divide" || op2 == " divide " || op2 == "divide by" || op2 == " divide by " ) {
-            return [ divOp2, "/", "Division: " ];
-        } else {
-            return false;
-        }
-    }
-
-    // module: output
-    // output2( "Test" );
-    function output2( outputData2 ){ 
-        console.log( outputData2 );
-    }
+    // function arrayContains( op2, arr2 ) {
+    //     return (arrhaystack.indexOf(needle) > -1);
+    // }
